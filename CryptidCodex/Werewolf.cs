8bec751f4578eae352041820db858c73c4020155
VacaSama <klc_moore@outlook.com>
2025-05-22 17:13:36 -0700

    Werewolf class made, with random generation

---
+++ CryptidCodex/Werewolf.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptidCodex
+{
+    public class Werewolf
+    {
+
+        public string Name { get; set; }
+        public string Encounter { get; set; }
+        public Werewolf()
+        {
+            getRandomWerewolf();
+        }
+
+        private void getRandomWerewolf()
+        {
+            string[] commonWerewolves = { "Fang", "Luna", "Dustpaw" };
+            string[] uncommonWerewolves = { "Lykaon", "Moonshade Howler", };
+            string[] rareWerewolves = { "Fenrir", "Bloodfang", "Shade Hunter" };
+            string[] epicWerewolves = { "Alpha", "Direfang", "Lupin (Cursed)" };
+
+            string[] rarities = { "Common", "Uncommon", "Rare", "Epic" };
+            Random rand = new Random();
+            Encounter = rarities[rand.Next(rarities.Length)];
+
+            if (Encounter == "Common")
+            {
+                Name = commonWerewolves[rand.Next(commonWerewolves.Length)];
+                Encounter = "Common";
+            }
+            else if (Encounter == "Uncommon")
+            {
+                Name = uncommonWerewolves[rand.Next(uncommonWerewolves.Length)];
+                Encounter = "Uncommon";
+            }
+            else if (Encounter == "Rare")
+            {
+                Name = rareWerewolves[rand.Next(rareWerewolves.Length)];
+                Encounter = "Rare";
+            }
+            else if (Encounter == "Epic")
+            {
+                Name = epicWerewolves[rand.Next(epicWerewolves.Length)];
+                Encounter = "Epic";
+            }
+
+        }
+    }
+}
+
+    }
+}
